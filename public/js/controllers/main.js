c9a9578df171440ddb06d6bd4e0a18d34527242e
Joshua Carrafa <carrafa@gmail.com>
2016-01-28 15:30:51 -0500

    adds mixer

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -32,6 +32,20 @@ ctrl.controller('main', [
       artwork: 'defaultArtwork'
     }, {}]
 
+    $scope.loadMixer = function() {
+      console.log('clicked.');
+      // var mixArray = [];
+      // mixArray.push(this.song.audio.track01);
+      // mixArray.push(this.song.audio.track02);
+      // mixArray.push(this.song.audio.track03);
+      // mixArray.push(this.song.audio.track04);
+      mixArray = ["http://localhost:8080/songs/default_song/drums.mp3",
+        "http://localhost:8080/songs/default_song/guitar.mp3",
+        "http://localhost:8080/songs/default_song/keys.mp3",
+        "http://localhost:8080/songs/default_song/lead.mp3"
+      ]
+      createTracks(mixArray);
+    }
 
     $scope.loadUser = function() {
       usersApi.loadUser($scope.cookie).then(function(response) {
@@ -54,6 +68,78 @@ ctrl.controller('main', [
 
     $scope.loadUser();
     $scope.loadSongs();
+
+    console.log('hi, nosy person')
+
+    $(function() {
+
+    })
+
+    var tracks = [
+      'audio/drums.mp3',
+      'audio/keys.mp3',
+      'audio/guitar.mp3',
+      'audio/lead.mp3'
+    ];
+
+    function newHowl(url) {
+      var howl = new Howl({
+        urls: [url],
+        autoplay: false,
+        buffer: true
+      });
+      return howl
+    };
+
+    function createTracks(tracks) {
+      var $pos = $('<input type="range">').attr('id', 'pos');
+      $('.controls').append($pos);
+      for (i = 0; i < tracks.length; i++) {
+
+        var $trackLabel = $('<div>').text('track' + i);
+        var $sliderContainer = $('<div>').addClass("slider-container");
+        var $track = $('<input type="range">').addClass("slider").attr('id',
+          'track' + i);
+
+        $($sliderContainer).append($track);
+        $($sliderContainer).append($trackLabel);
+        $('.mixer').append($sliderContainer);
+
+        var howl = newHowl(tracks[i]);
+
+        $pos.attr('max', howl);
+        addTrackToSlider($track, howl);
+        addTrackToControls(howl);
+
+      };
+    };
+
+    function addTrackToControls(howl) {
+      $('#play').on('click', function() {
+        if (howl.paused === false) {
+          console.log('no');
+        } else {
+          howl.play();
+          console.log('play');
+        }
+      });
+      $('#pause').on('click', function() {
+        howl.pause();
+      });
+      $('#stop').on('click', function() {
+        howl.stop();
+      });
+      $('#pos').on("change", function() {
+        howl.pos($(this).val());
+      });
+
+    };
+
+    function addTrackToSlider(input, howl) {
+      input.on("mousemove", function() {
+        howl.volume($(this).val() / 100);
+      });
+    };
 
   }
 ]);
